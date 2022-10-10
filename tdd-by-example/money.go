60d16a1990312000d973def2632c66dd361c4747
Siri P R <siri@nilenso.com>
2022-10-10 17:01:59 +0530

    Test and implement Franc struct

--- tdd-by-example/money.go
+++ tdd-by-example/money.go
@@ -4,6 +4,10 @@ type Dollar struct {
 	amount int
 }
 
+type Franc struct {
+	amount int
+}
+
 func newDollar(amount int) *Dollar {
 	return &Dollar{amount: amount}
 }
@@ -15,4 +19,17 @@ func (d *Dollar) times(multiplier int) *Dollar {
 func (d *Dollar) equals(object interface{}) bool {
 	dollar := object.(*Dollar)
 	return d.amount == dollar.amount
+}
+
+func newFranc(amount int) *Franc {
+	return &Franc{}
+}
+
+func (f *Franc) times(multiplier int) *Franc {
+	return newFranc(f.amount * multiplier)
+}
+
+func (f *Franc) equals(object interface{}) bool {
+	franc := object.(*Franc)
+	return f.amount == franc.amount
 }
