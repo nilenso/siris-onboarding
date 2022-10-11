fc4f168c59a72cee47ef18a16cfd37121208b903
Siri P R <siri@nilenso.com>
2022-10-11 14:12:52 +0530

    Remove redundant tests

--- tdd-by-example/money_test.go
+++ tdd-by-example/money_test.go
@@ -10,33 +10,18 @@ func TestMultiplication(t *testing.T) {
 	five := newDollar(5)
 	assert.True(t, newDollar(10).equals(five.times(2)))
 	assert.True(t, newDollar(15).equals(five.times(3)))
-	assert.False(t, newFranc(5).equals(five.times(1)))
 }
 
 func TestEquality(t *testing.T) {
 	assert.True(t, newDollar(5).equals(newDollar(5)))
 	assert.False(t, newDollar(5).equals(newDollar(6)))
-	assert.True(t, newFranc(5).equals(newFranc(5)))
-	assert.False(t, newFranc(5).equals(newFranc(6)))
 	assert.False(t, newDollar(5).equals(newFranc(5)))
 }
 
-func TestDifferentStructEquality(t *testing.T) {
-	assert.True(t, newFranc(1).equals(newMoney(1, "CHF")))
-}
-
-func TestFrancMultiplication(t *testing.T) {
-	five := newFranc(5)
-	assert.True(t, newFranc(10).equals(five.times(2)))
-	assert.True(t, newFranc(15).equals(five.times(3)))
-}
-
 func TestAmount(t *testing.T) {
-	five := newDollar(5)
-	assert.Equal(t, 5, five.Amount())
+	assert.Equal(t, 5, newDollar(5).Amount())
 }
 
 func TestCurrency(t *testing.T) {
 	assert.Equal(t, "USD", newDollar(1).Currency())
-	assert.Equal(t, "CHF", newFranc(1).Currency())
 }
