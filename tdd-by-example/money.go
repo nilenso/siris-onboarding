7f403fbe6556e573af05bd2dcdbdc5a2a38991fc
Siri P R <siri@nilenso.com>
2022-10-11 13:27:19 +0530

    Add abstraction to Money struct with IMoney interface

--- tdd-by-example/money.go
+++ tdd-by-example/money.go
@@ -8,33 +8,42 @@ type Franc struct {
 	Money
 }
 
+type IMoney interface {
+	equals(money IMoney) bool
+	times(multiplier int) IMoney
+	Amount() int
+	Currency() string
+}
+
 type Money struct {
-	amount int
+	amount   int
+	currency string
 }
 
-func newDollar(amount int) *Dollar {
-	return &Dollar{Money{amount: amount}}
+func newDollar(amount int) IMoney {
+	return &Dollar{Money{amount: amount, currency: "USD"}}
 }
 
-func (d *Dollar) times(multiplier int) *Dollar {
-	return newDollar(d.amount * multiplier)
+func newFranc(amount int) IMoney {
+	return &Franc{Money{amount: amount, currency: "CHF"}}
 }
 
-func (d *Dollar) equals(object interface{}) bool {
-	dollar := object.(*Dollar)
-	return d.amount == dollar.amount
+func newMoney(amount int) IMoney {
+	return &Money{amount: amount}
 }
 
-func newFranc(amount int) *Franc {
-	return &Franc{Money{amount: amount}}
+func (m *Money) equals(money IMoney) bool {
+	return m.amount == money.Amount() && m.currency == money.Currency()
 }
 
-func (f *Franc) times(multiplier int) *Franc {
-	return newFranc(f.amount * multiplier)
+func (m *Money) times(multiplier int) IMoney {
+	return newMoney(m.amount * multiplier)
 }
 
-func (f *Franc) equals(object interface{}) bool {
-	franc := object.(*Franc)
-	return f.amount == franc.amount
+func (m *Money) Amount() int {
+	return m.amount
 }
 
+func (m *Money) Currency() string {
+	return m.currency
+}
--- tdd-by-example/money_test.go
+++ tdd-by-example/money_test.go
@@ -8,8 +8,8 @@ import (
 
 func TestMultiplication(t *testing.T) {
 	five := newDollar(5)
-	assert.Equal(t, newDollar(10), five.times(2))
-	assert.Equal(t, newDollar(15), five.times(3))
+	assert.True(t, newDollar(10).equals(five.times(2)))
+	assert.True(t, newDollar(15).equals(five.times(3)))
 }
 
 func TestEquality(t *testing.T) {
@@ -17,10 +17,21 @@ func TestEquality(t *testing.T) {
 	assert.False(t, newDollar(5).equals(newDollar(6)))
 	assert.True(t, newFranc(5).equals(newFranc(5)))
 	assert.False(t, newFranc(5).equals(newFranc(6)))
+	assert.False(t, newDollar(5).equals(newFranc(5)))
 }
 
 func TestFrancMultiplication(t *testing.T) {
 	five := newFranc(5)
-	assert.Equal(t, newFranc(10), five.times(2))
-	assert.Equal(t, newFranc(15), five.times(3))
+	assert.True(t, newFranc(10).equals(five.times(2)))
+	assert.True(t, newFranc(15).equals(five.times(3)))
+}
+
+func TestAmount(t *testing.T) {
+	five := newDollar(5)
+	assert.Equal(t, 5, five.Amount())
+}
+
+func TestCurrency(t *testing.T) {
+	assert.Equal(t, "USD", newDollar(1).Currency())
+	assert.Equal(t, "CHF", newFranc(1).Currency())
 }
