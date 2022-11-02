c522329778002ff60a00b496a7e4076d12829c69
Siri P R <siri@nilenso.com>
2022-11-02 19:44:09 +0530

    Add tests and implement move and turn-90 functions

--- mars-rover-clj/src/mars_rover_clj/core.clj
+++ mars-rover-clj/src/mars_rover_clj/core.clj
@@ -1,7 +1,64 @@
 (ns mars-rover-clj.core)
+(require '(clojure.set))
+
+(def directions {"N" 0
+                 "E" 90
+                 "S" 180
+                 "W" 270})
+
+(def directions-map {"N" {:angle 0
+                          :move  (fn [x y heading] [x (inc y) heading])}
+                     "E" {:angle 90
+                          :move  (fn [x y heading] [(inc x) y heading])}
+                     "S" {:angle 180
+                          :move  (fn [x y heading] [x (dec y) heading])}
+                     "W" {:angle 270
+                          :move  (fn [x y heading] [(dec x) y heading])}})
+
+(defn turn-left
+  "Returns an angle of the compass point after turning left by the given angle."
+  [heading turn-by]
+  (-
+    ((directions-map heading) :angle)
+    turn-by))
+
+(defn turn-right
+  "Returns an angle of the compass point after turning right by the given angle."
+  [heading turn-by]
+  (+
+    ((directions-map heading) :angle)
+    turn-by))
+
+(def turn-directions {"L" (fn [heading turn-by]
+                            (turn-left heading turn-by))
+                      "R" (fn [heading turn-by]
+                            (turn-right heading turn-by))})
+
+(defn angle-to-direction
+  "Returns a compass point given the angle. North 0, East 90, South 180, West 270."
+  [angle]
+  ((clojure.set/map-invert directions)
+   (mod angle 360)))
 
 (defn move
   "Returns the new position (x and y coordinates) and the heading of the rover"
-  [x y direction]
-  ;get move function
-  '(1 3 "N"))
+  [x y heading]
+  (cond (= heading "N")
+        [x (inc y) heading]
+        (= heading "S")
+        [x (dec y) heading]
+        (= heading "E")
+        [(inc x) y heading]
+        (= heading "W")
+        [(dec x) y heading]))
+
+(defn turn-90
+  "Turns the heading of the rover in the given direction"
+  [heading turn-direction]
+  (angle-to-direction
+    ((turn-directions turn-direction)
+     heading
+     90)))
+
+(turn-90 "N" "R")
+(angle-to-direction 0)
--- mars-rover-clj/test/mars_rover_clj/core_test.clj
+++ mars-rover-clj/test/mars_rover_clj/core_test.clj
@@ -1,7 +1,16 @@
 (ns mars-rover-clj.core-test
-  (:require [clojure.test :refer :all]
-            [mars-rover-clj.core :refer :all]))
+  (:require [clojure.set :refer :all]
+            [clojure.test :refer [deftest is testing]]
+            [mars-rover-clj.core :refer [move turn-90]]))
 
 (deftest move-test
   (testing "Move returns the new co-ordinates and heading"
-    (is (= (move 1 2 "N") '(1 3 "N")))))
+    (is (= (move 1 2 "N") [1 3 "N"]))))
+
+(deftest turn-90-test
+  (testing "Tests for the correctness of turn-90"
+    (is (= (turn-90 "N" "R") "E"))))
+
+;(deftest turn-left
+;  (testing "Tests for the correctness of turn-left"
+;    (is (= (turn-left )))))
