76cd25224b218e95755078f73cd6243c1f74de37
Siri P R <siri@nilenso.com>
2022-11-09 15:55:40 +0530

    Decouple check bounds from move

--- mars-rover-clj/src/mars_rover_clj/movement.clj
+++ mars-rover-clj/src/mars_rover_clj/movement.clj
@@ -11,6 +11,10 @@
    "W" {:compass-angle 270
         :move-f        (fn [rover] (update rover :x dec))}})
 
+(def rover-out-of-bounds-error
+  "Error string when a bad instruction is passed to rover making it go off-grid."
+  "Bad instructions. Rover overshoots plateau bounds.")
+
 (def
   ^{:doc "Returns the direction (N, E, S or W) from the compass angle"}
   compass-angle-to-direction
@@ -29,7 +33,7 @@
 ; Rover movements
 (defn check-bounds
   "Returns true if the rover's position is within the plateau bounds, false otherwise"
-  [plateau-bounds rover]
+  [rover plateau-bounds]
   (and
     (<= 0 (:x rover) (first plateau-bounds))
     (<= 0 (:y rover) (second plateau-bounds))))
@@ -37,21 +41,31 @@
 (defn move
   "Returns a rover with updated position (x and y coordinates) and the heading.
   If rover position results beyond the plateau bounds, it returns the rover unchanged."
-  [plateau-bounds rover]
-  (let [moved-rover ((:move-f (directions (:heading rover))) rover)]
-    (if (check-bounds plateau-bounds moved-rover)
-      moved-rover
-      rover)))
+  [rover]
+  ((:move-f (directions (:heading rover))) rover))
 
 (def rover-movements
   "A map of instruction symbols and the movement functions to move rovers"
-  {"L" (fn [_ rover]
+  {"L" (fn [rover]
          (update rover :heading #(turn-rover-heading % - 90)))
-   "R" (fn [_ rover]
+   "R" (fn [rover]
          (update rover :heading #(turn-rover-heading % + 90)))
    "M" move})
 
+(defn move-rover
+  "Moves a rover as specified by the instructions string"
+  [rover instructions plateau-bounds]
+  (if (not-empty instructions)
+    (move-rover
+      ((rover-movements (first instructions)) rover)
+      (rest instructions)
+      plateau-bounds)
+    (if (check-bounds rover plateau-bounds)
+      rover
+      rover-out-of-bounds-error)))
+
 ; I/O functions
+
 (defn init-rovers
   "Takes an input for the Mars Rover problem
   as a vector of the plateau's upper right coordinates and an infinite sequence of rover's initial position
@@ -62,74 +76,19 @@
    :rovers         (->> (partition 2 rover-details)
                      (map (fn [rover-instructions]
                             (let [[x y heading] (first rover-instructions)]
-                              [{:x       x
-                                :y       y
-                                :heading heading}
-                               (second rover-instructions)]))))})
+                              {:rover        {:x       x
+                                              :y       y
+                                              :heading heading}
+                               :instructions (->>
+                                               (second rover-instructions)
+                                               (map str)
+                                               (vec))}))))})
 
-
-(defn move-rover
-  "Moves a rover as specified by the instructions string"
-  [plateau-bounds [rover instructions]]
-  (reduce (fn [acc instruction]
-            ((->>
-               (str instruction)
-               (rover-movements)) plateau-bounds acc))
-          rover instructions))
-
-(defn move-rovers
+(defn move-rovers-sequentially
   "Moves rovers based on the instruction string"
-  [rover-input]
-  (->> (rover-input :rovers)
-    (map move-rover (:plateau-bounds rover-input))))
-
-;Testing stuff
-
-(move [5 5] {:x       1
-             :y       2
-             :heading "N"})
-
-(((directions "N") :move-f) {:x       1
-                             :y       2
-                             :heading "N"})
-(->> (partition 2 [[1 2 "N"]
-                   "LMLMLMLMM"
-                   [3 3 "E"]
-                   "MMRMMRMRRM"])
-  (map vec))
-(move-rovers (init-rovers [5 5]
-                          [1 2 "N"]
-                          "LMLMLMLMM"
-                          [3 3 "E"]
-                          "MMRMMRMRRM"))
-
-(move-rover [5 5] [{:x       1
-                    :y       2
-                    :heading "N"} "LMLMLMLMM"])
-(move-rover [5 5] [{:x       3
-                    :y       3
-                    :heading "E"} "MMRMMRMRRM"])
-
-(compass-angle-to-direction 180)
-(map str "MMRMMRMRRM")
-(turn-rover-heading "N" - 90)
-
-
-(map
-  (fn [[[x y heading] [movement-instructions]]]
-    (let [rover {:x       x
-                 :y       y
-                 :heading heading}]
-      (map
-        #(->>
-           (str %)
-           ((rover-movements) rover))
-        movement-instructions))))
-(move [5 5] {:x 5, :y 3, :heading "E"})
-
-((->>
-   (str "M")
-   (rover-movements))
- [5 5] {:x 4, :y 3, :heading "E"})
-
-(compass-angle-to-direction 90)
+  [{:keys [plateau-bounds rovers]}]
+  (map #(move-rover
+          (:rover %)
+          (:instructions %)
+          plateau-bounds)
+       rovers))
--- mars-rover-clj/test/mars_rover_clj/movement_test.clj
+++ mars-rover-clj/test/mars_rover_clj/movement_test.clj
@@ -5,32 +5,32 @@
 (deftest move-test
   (testing "Move returns the new co-ordinates and heading"
     (is (=
-          (movement/move [5 5] {:x 1 :y 2 :heading "N"})
+          (movement/move {:x 1 :y 2 :heading "N"})
           {:heading "N"
            :x       1
            :y       3}))
     (is (=
-          (movement/move [5 5] {:x 1 :y 2 :heading "S"})
+          (movement/move {:x 1 :y 2 :heading "S"})
           {:heading "S"
            :x       1
            :y       1}))
     (is (=
-          (movement/move [5 5] {:x 5 :y 2 :heading "N"})
-          {:heading "N"
-           :x       5
+          (movement/move {:x 5 :y 2 :heading "E"})
+          {:heading "E"
+           :x       6
            :y       2}))))
 
 (deftest check-bounds-test
-  (is (= (movement/check-bounds [5 5] {:heading "N"
-                                       :x       3
-                                       :y       4})
-         {:heading "N"
-          :x       3
-          :y       4}))
-  (is (= (movement/check-bounds [5 5] {:heading "N"
-                                       :x       6
-                                       :y       4})
-         nil))
+  (is (= (movement/check-bounds {:heading "N"
+                                 :x       3
+                                 :y       4}
+                                [5 5])
+         true))
+  (is (= (movement/check-bounds {:heading "N"
+                                 :x       6
+                                 :y       4}
+                                [5 5])
+         false))
   )
 
 (deftest compass-angle-to-direction-test
@@ -66,14 +66,40 @@
           (movement/turn-rover-heading "S" - 90)
           "E"))))
 
-(deftest move-rovers-test
+(deftest move-rover-test
+  (testing "Correctness of move-rover"
+    (is (=
+          (movement/move-rover
+            {:x 1, :y 3, :heading "N"}
+            ["L" "M" "L" "M" "L" "M" "L" "M" "M"]
+            [5 5])
+          {:x       1
+           :y       4
+           :heading "N"}))
+    (is (=
+          (movement/move-rover
+            {:x 3, :y 3, :heading "E"}
+            ["M" "M" "R" "M" "M" "R" "M" "R" "R" "M"]
+            [5 5])
+          {:x       5
+           :y       1
+           :heading "E"}))
+    (is (=
+          (movement/move-rover
+            {:x 3, :y 3, :heading "E"}
+            ["M" "M" "M" "L" "M" "R" "M" "R" "R" "M"]
+            [5 5])
+          movement/rover-out-of-bounds-error)))
+  )
+
+(deftest move-rovers-sequentially-test
   (testing "move-rovers"
     (is (= (->> (movement/init-rovers [5 5]
                                       [1 2 "N"]
                                       "LMLMLMLMM"
                                       [3 3 "E"]
                                       "MMRMMRMRRM")
-             (movement/move-rovers))
+             (movement/move-rovers-sequentially))
            '({:x 1, :y 3, :heading "N"} {:x 5, :y 1, :heading "E"})))))
 
 (deftest init-rovers-test
@@ -85,5 +111,8 @@
                                  "MMRMMRMRRM")
            '{:plateau-bounds [5 5],
              :rovers         (
-                              [{:x 1, :y 2, :heading "N"} "LMLMLMLMM"]
-                              [{:x 3, :y 3, :heading "E"} "MMRMMRMRRM"])}))))
+                              {:rover        {:x 1, :y 2, :heading "N"}
+                               :instructions ["L" "M" "L" "M" "L" "M" "L" "M" "M"]}
+                              {
+                               :rover        {:x 3, :y 3, :heading "E"}
+                               :instructions ["M" "M" "R" "M" "M" "R" "M" "R" "R" "M"]})}))))
