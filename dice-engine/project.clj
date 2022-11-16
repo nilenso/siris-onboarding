4c67125fd483a16012e74d91c93b732ddefd806f
Siri P R <siri.pr914@gmail.com>
2022-11-16 13:02:20 +0530

    feat(project): add dice-engine project

---
+++ dice-engine/project.clj
@@ -0,0 +1,7 @@
+(defproject dice-engine "0.1.0-SNAPSHOT"
+  :description "Solution to the Dice Engine problem."
+  :url ""
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :repl-options {:init-ns dice-engine.core})
---
+++ dice-engine/src/dice_engine/core.clj
@@ -0,0 +1,7 @@
+(ns dice-engine.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
+
