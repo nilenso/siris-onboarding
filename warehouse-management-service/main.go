f132605bb1fb5cff7930a991333288ec8eec22d2
Siri P R <siri.pr914@gmail.com>
2023-01-12 19:33:46 +0530

    chore: remove redundant comments from main

--- warehouse-management-service/main.go
+++ warehouse-management-service/main.go
@@ -17,27 +17,23 @@ const EnvConfigFilePath = "CONFIG_FILE_PATH"
 func main() {
 	logger := log.New(log.Warning)
 
-	// Get absolute path of config file from env variable
 	configFilePath, ok := os.LookupEnv(EnvConfigFilePath)
 	if !ok {
 		logger.Log(log.Fatal, fmt.Sprintf("%s env variable not set", EnvConfigFilePath))
 	}
 
-	// Get config
 	config, err := config.FromFile(configFilePath)
 	if err != nil {
 		logger.Log(log.Fatal, fmt.Sprintf("App startup error: %v", err))
 		os.Exit(1)
 	}
 
-	// Get Postgres DB service
 	db, err := postgres.New(config.Postgres)
 	if err != nil {
 		logger.Log(log.Fatal, fmt.Sprintf("App startup error: %v", err))
 		os.Exit(1)
 	}
 
-	// Close DB connection at shutdown
 	defer func() {
 		err = db.Close()
 		if err != nil {
