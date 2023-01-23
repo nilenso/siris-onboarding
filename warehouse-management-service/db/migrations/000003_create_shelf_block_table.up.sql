67b1dc405eaaf70dde09d3170a2673821d05deba
Siri P R <siri.pr914@gmail.com>
2023-01-23 16:42:53 +0530

    fix: add missing warehouse relationship to shelf_block

--- warehouse-management-service/db/migrations/000003_create_shelf_block_table.up.sql
+++ warehouse-management-service/db/migrations/000003_create_shelf_block_table.up.sql
@@ -2,5 +2,6 @@ CREATE TABLE IF NOT EXISTS shelf_block(
     id TEXT PRIMARY KEY DEFAULT gen_random_uuid (),
     aisle TEXT,
     rack TEXT,
-    storage_type TEXT
+    storage_type TEXT,
+    warehouse_id TEXT
 );
